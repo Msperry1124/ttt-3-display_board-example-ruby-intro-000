19844bc04a81896a8abdef185dc088d35b0af615
Melissa Sperry <sperry.mel@gmail.com>
2020-02-10 04:45:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@ def display_board
   cell = "   |   |   "
   row = "-----------"
   puts "A Tic Tac Toe Board"
-  print(cell)
-  print(row)
-  print(cell)
-  print(row)
-  print(cell)
+  print (cell)
+  print (row)
+  print (cell)
+  print (row)
+  print (cell)
 end
